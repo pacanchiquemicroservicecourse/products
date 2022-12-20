0a42527e9315fc17a326d391284179761baab8f3
Javier-Personal <javier.pacanchique.leon@gmail.com>
2022-12-20 17:59:41 -0500

    Base line

---
+++ src/test/java/com/app/pacanchique/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pacanchique.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
