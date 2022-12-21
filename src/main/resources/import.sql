d0acb7a678a80c65a8e31572ca9f995f7db264d7
Javier-Personal <javier.pacanchique.leon@gmail.com>
2022-12-20 21:28:14 -0500

    Import products data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, price, create) VALUES ('PANASONIC', 800, NOW());
+INSERT INTO products (name, price, create) VALUES ('SONY', 1800, NOW());
+INSERT INTO products (name, price, create) VALUES ('APPLE', 564, NOW());
+INSERT INTO products (name, price, create) VALUES ('SAMSUNG', 54567, NOW());
+INSERT INTO products (name, price, create) VALUES ('ADIDAS', 2800, NOW());
+INSERT INTO products (name, price, create) VALUES ('REBOOK', 6700, NOW());
